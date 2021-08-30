18e23d871b70bba8fb2cb6cbad6c37231f133344
Hisam A Fahri <hisamafahri@ritelin.co.id>
2021-08-30 18:59:30 +0700

    feat(home): add slatejs
    I have added barebone slatejs

--- components/StyleBar.jsx
+++ components/StyleBar.jsx
@@ -4,22 +4,20 @@ import { BoldIcon, ItalicIcon, UnderlineIcon, LeftAlignIcon, CenterAlignIcon, Ri
 
 function StyleBar() {
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <div className='flex flex-row items-center justify-center px-9 py-4 my-2 w-min'>
-                <StyleButton icon={<BoldIcon />} />
-                <StyleButton icon={<ItalicIcon />} />
-                <StyleButton icon={<UnderlineIcon />} />
-                <div className='mx-6 h-12 border-editr-light-grey border border-l-0.5'></div>
-                <StyleButton icon={<LeftAlignIcon />} />
-                <StyleButton icon={<CenterAlignIcon />} />
-                <StyleButton icon={<RightAlignIcon />} />
-                <div className='mx-6 h-12 border-editr-light-grey border border-l-0.5'></div>
-                <StyleButton icon={<H1Icon />} />
-                <StyleButton icon={<H2Icon />} />
-                <div className='mx-6 h-12 border-editr-light-grey border border-l-0.5'></div>
-                <StyleButton icon={<InlineCodeIcon />} />
-                <StyleButton icon={<CodeBlockIcon />} />
-            </div>
+        <div className='flex flex-row items-center justify-center px-9 py-4 my-2 w-min'>
+            <StyleButton icon={<BoldIcon />} />
+            <StyleButton icon={<ItalicIcon />} />
+            <StyleButton icon={<UnderlineIcon />} />
+            <div className='mx-6 h-12 border-editr-light-grey border border-l-0.5'></div>
+            <StyleButton icon={<LeftAlignIcon />} />
+            <StyleButton icon={<CenterAlignIcon />} />
+            <StyleButton icon={<RightAlignIcon />} />
+            <div className='mx-6 h-12 border-editr-light-grey border border-l-0.5'></div>
+            <StyleButton icon={<H1Icon />} />
+            <StyleButton icon={<H2Icon />} />
+            <div className='mx-6 h-12 border-editr-light-grey border border-l-0.5'></div>
+            <StyleButton icon={<InlineCodeIcon />} />
+            <StyleButton icon={<CodeBlockIcon />} />
         </div>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,34 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import StyleBar from '../components/StyleBar'
+import React, { useMemo, useState } from 'react'
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
 
 
 export default function Home() {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph. A line of text in a paragraph. A line of text in a paragraph. A line of text in a paragraph. A line of text in a paragraph. A line of text in a paragraph. A line of text in a paragraph. A line of text in a paragraph. ' }],
+    },
+  ])
   return (
     <>
       <Head>
         <title>Editr. - yet another simple Rich Text Editor</title>
       </Head>
       <Header />
-      <StyleBar />
+      <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center w-min'>
+          <StyleBar />
+          <hr className='bg-editr-light-grey h-0.5 w-full mt-2 mb-12' />
+          <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+            <Editable className='font-light font-rubik w-full' />
+          </Slate>
+        </div>
+      </div>
     </>
   )
 }
